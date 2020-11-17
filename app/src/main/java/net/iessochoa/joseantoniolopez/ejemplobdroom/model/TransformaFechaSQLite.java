d8256db6a0d304391f731a603e2e2081c1a78914
JoseA <iessochoalmj201617@gmail.com>
2020-11-17 16:01:50 +0100

    corregido fecha

--- app/src/main/java/net/iessochoa/joseantoniolopez/ejemplobdroom/model/TransformaFechaSQLite.java
+++ app/src/main/java/net/iessochoa/joseantoniolopez/ejemplobdroom/model/TransformaFechaSQLite.java
@@ -2,6 +2,7 @@ package net.iessochoa.joseantoniolopez.ejemplobdroom.model;
 
 import androidx.room.TypeConverter;
 
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -13,9 +14,18 @@ public class TransformaFechaSQLite {
     public static Date fromTimestamp(Long value) {
         return value == null ? null : new Date(value);
     }
-
     @TypeConverter
     public static Long dateToTimestamp(Date date) {
-        return date == null ? null : date.getTime();
+        //le quitamos la hora, pero si la necesitárais, no llaméis a removeTime
+        return date == null ? null : removeTime(date).getTime();
+    }
+    public static Date removeTime(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        return cal.getTime();
     }
 }
