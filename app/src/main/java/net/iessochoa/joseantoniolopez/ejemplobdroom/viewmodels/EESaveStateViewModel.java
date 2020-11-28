73c4b7c41a587c82b30ae487dd861b9790c4aae6
JoseA <david2009.lopez@gmail.com>
2020-11-28 11:38:26 +0100

    savestate

--- app/src/main/java/net/iessochoa/joseantoniolopez/ejemplobdroom/viewmodels/EESaveStateViewModel.java
+++ app/src/main/java/net/iessochoa/joseantoniolopez/ejemplobdroom/viewmodels/EESaveStateViewModel.java
@@ -13,18 +13,30 @@ import net.iessochoa.joseantoniolopez.ejemplobdroom.model.Contacto;
 import net.iessochoa.joseantoniolopez.ejemplobdroom.repository.ContactoRepository;
 
 import java.util.List;
-//matar a proceso en android:
-// c:\Android\Sdk\platform-tools>adb shell am force-stop net.iessochoa.joseantoniolopez.ejemplobdroom
-//https://developer.android.com/topic/libraries/architecture/viewmodel-savedstate
-//https://codelabs.developers.google.com/codelabs/android-lifecycles/#6
+/*Cuando el proceso de la app es destruido por necesidad de recursos, el ViewModel es
+destruido también. Podemos guardar en SavedStateHandle la información que queramos
+para no perder la experiencia de usuario.
+el los siguiente artículos tenéis más información
+https://developer.android.com/topic/libraries/architecture/viewmodel-savedstate
+https://codelabs.developers.google.com/codelabs/android-lifecycles/#6
+ matar a proceso en android:
+minimiza la app con botón home
+En consola vamos al direcctorio del sdk \Android\Sdk\platform-tools>
+adb shell
+su
+ ps -A | grep ejemplodbroom
+kill numero_proceso
+//*/
 public class EESaveStateViewModel  extends AndroidViewModel {
+    //los valores los guardamos como clave/valor
     private static final String STATUS_BUSQUEDA="campo_busqueda";
+
     private ContactoRepository mRepository;
     //LiveData que depende de otros elementos
     private MutableLiveData<String> condicionBusquedaLiveData;
     // Lista de contactos devuelto por sql mediante Room;
     private LiveData<List<Contacto>> listaContactosLiveData;
-    //añadadimos el estado
+    //El SavedStateHandle me permite recuperar los datos ante una destrucción del proceso
     private SavedStateHandle mState;
 //añadimos un nueva constructor
 public EESaveStateViewModel(@NonNull Application application,SavedStateHandle savedStateHandle) {
@@ -35,29 +47,23 @@ public EESaveStateViewModel(@NonNull Application application,SavedStateHandle sa
     //Este Livedata estará asociado al editext de busqueda por nombre
     condicionBusquedaLiveData=new MutableLiveData<String>();
     //en primer momento no hay condición
-    condicionBusquedaLiveData.setValue("");
-
+    // si venimos de una reconstrucción la recuperamos
+    String condicion=mState.get(STATUS_BUSQUEDA);
+    if(condicion==null)
+        setCondicionBusqueda("");
+    else
+        setCondicionBusqueda(condicion);
     //version lambda
     listaContactosLiveData= Transformations.switchMap(condicionBusquedaLiveData, nombre -> mRepository.getByNombre(nombre));
 }
-    /*public EESaveStateViewModel(@NonNull Application application) {
-        super(application);
-        mRepository=ContactoRepository.getInstance(application);
 
-        //Este Livedata estará asociado al editext de busqueda por nombre
-        condicionBusquedaLiveData=new MutableLiveData<String>();
-        //en primer momento no hay condición
-        condicionBusquedaLiveData.setValue("");
-        
-        //version lambda
-        listaContactosLiveData= Transformations.switchMap(condicionBusquedaLiveData, nombre -> mRepository.getByNombre(nombre));
-    }*/
     public LiveData<List<Contacto>> getByNombre()
     {
         return listaContactosLiveData;
     }
     public void setCondicionBusqueda(String condicionBusqueda) {
-
+    //si cambia la condición de búsqueda, la guardamos en el SaveState
+        mState.set(STATUS_BUSQUEDA,condicionBusqueda);
         condicionBusquedaLiveData.setValue(condicionBusqueda);
     }
     //Inserción y borrado que se reflejará automáticamente gracias al observador creado en la
